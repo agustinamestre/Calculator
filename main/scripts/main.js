75384afe01630d6cb61395f19b649a018b484215
Agustina <agustinamestre@gmail.com>
2021-01-18 11:26:21 -0300

    combined operations

--- main/scripts/main.js
+++ main/scripts/main.js
@@ -18,6 +18,10 @@ for (let i = 0; i <= 9; i++) {
 
 const allowMoreOperations = () => !(document.getElementById("calculator-output").innerHTML === "Error")
 
+const zeroScreen = () => !(document.getElementById("calculator-output").innerHTML === "0")
+
+const emptyScreen = () => !(document.getElementById("calculator-output").innerHTML === "")
+
 const isOperatorPressed = () => state.operator
 
 const trackNumberPressed = (operatorKeyName, pressedNumber) => {
@@ -30,9 +34,18 @@ const trackNumberPressed = (operatorKeyName, pressedNumber) => {
 
 numbers.forEach(n => {
   document.getElementById(n).addEventListener("click", (ev) => {
+    if (result.innerHTML === "0") {
+      result.innerHTML = "";
+    }
+
     if (allowMoreOperations()) {
-      const operandKeyName = isOperatorPressed() ? 'rightOperand' : 'leftOperand';
-      trackNumberPressed(operandKeyName, ev.target.innerHTML);
+      const operandKeyName = isOperatorPressed() && state.leftOperand != 0 ? 'rightOperand' : 'leftOperand';
+      if (state.operator === "-" && operandKeyName === 'leftOperand') {
+        trackNumberPressed(operandKeyName, result.innerHTML + ev.target.innerHTML);
+      } else {
+        trackNumberPressed(operandKeyName, ev.target.innerHTML);
+      }
+
       result.innerHTML = result.innerHTML + (ev.target.innerHTML);
     }
   })
@@ -41,16 +54,25 @@ numbers.forEach(n => {
 operations.forEach(o => {
   document.getElementById(o).addEventListener("click", (ev) => {
     if (allowMoreOperations()) {
-      result.innerHTML = result.innerHTML + ev.target.innerHTML;
+      if (emptyScreen() && zeroScreen()) {
+        result.innerHTML = result.innerHTML + ev.target.innerHTML;
+      }
 
       state.operator = ev.target.innerHTML;
 
-      if (isOperatorPressed() !== null) {
-        result.innerHTML = state.leftOperand + ev.target.innerHTML;
+      if (isOperatorPressed()) {
+        if (state.leftOperand === 0 && state.operator === "-") {
+          result.innerHTML = "-";
+        }
+        if (state.leftOperand != 0) {
+          result.innerHTML = state.leftOperand + ev.target.innerHTML;
+        }
       } else {
         result.innerHTML = state.rightOperand + ev.target.innerHTML;
       }
     }
+
+
   });
 });
 
@@ -58,10 +80,11 @@ document.getElementById("dot").addEventListener("click", dot => {
   if (allowMoreOperations()) {
     if (!isOperatorPressed() && !state.leftOperand.toString().includes(".")) {
       state.leftOperand += ".";
+      result.innerHTML = result.innerHTML + dot.target.innerHTML;
     } else if (isOperatorPressed() && !state.rightOperand.toString().includes(".")) {
       state.rightOperand += ".";
+      result.innerHTML = result.innerHTML + dot.target.innerHTML;
     }
-    result.innerHTML = result.innerHTML + dot.target.innerHTML;
   }
 })
 
@@ -69,6 +92,9 @@ equal.addEventListener("click", e => {
   if (allowMoreOperations()) {
     calc();
   }
+  state.leftOperand = result.innerHTML;
+  state.operator = null;
+  state.rightOperand = 0;
 })
 
 clearLastValue.addEventListener("click", e => {
@@ -81,7 +107,7 @@ clearAll.addEventListener("click", e => {
   state.leftOperand = 0;
   state.rightOperand = 0;
   state.operator = null;
-  result.innerHTML = " ";
+  result.innerHTML = "0";
 })
 
 function calc() {
